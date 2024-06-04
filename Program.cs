71f97c9bd1ce446dddacbf6482af97fc70da43f7
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-04 15:28:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment_5._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "fly me to the moon";
+
+            int count = 0;
+
+            for (int i = s.Length - 1; i > 0; i--) 
+            {
+                if (s[i].ToString() != " ")
+                {
+                    count++;
+                }
+                else 
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(count);   
+        }
+    }
+}
